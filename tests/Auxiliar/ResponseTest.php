36c952c05f511c14590c7a15370335f2bcc24c7f
Roberto L. Machado <linux.rlm@gmail.com>
2016-06-26 11:36:32 -0300

    [UPD] tests

---
+++ tests/Auxiliar/ResponseTest.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Class ResponseTest
+ * @author Roberto L. Machado <linux.rlm at gmail dot com>
+ */
+use NFePHP\MDFe\Response;
+
+class ResponseTest extends PHPUnit_Framework_TestCase
+{
+    public $mdfe;
+    
+    public function testeInstanciar()
+    {
+        $this->mdfe = new Response();
+    }
+}
