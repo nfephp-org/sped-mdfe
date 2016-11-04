694a542f6c7cd68abde437f7c178ff56a8988af2
Gleidson Brito Santana <gleidsonbrito.ads@gmail.com>
2016-11-04 12:53:07 -0200

    Declaração de namespace nos Casos de Teste.

--- tests/Auxiliar/ResponseTest.php
+++ tests/Auxiliar/ResponseTest.php
@@ -1,15 +1,17 @@
 <?php
 
+namespace Tests\NFePHP\MDFe\Auxiliar;
+
 /**
- * Class ResponseTest
  * @author Roberto L. Machado <linux.rlm at gmail dot com>
  */
 use NFePHP\MDFe\Auxiliar\Response;
+use PHPUnit_Framework_TestCase;
 
 class ResponseTest extends PHPUnit_Framework_TestCase
 {
     public $mdfe;
-    
+
     public function testInstanciar()
     {
         $this->mdfe = new Response();
--- tests/MailTest.php
+++ tests/MailTest.php
@@ -1,20 +1,22 @@
 <?php
 
+namespace Tests\NFePHP\MDFe;
+
 /**
- * Class MailMDFeTest
  * @author Roberto L. Machado <linux.rlm at gmail dot com>
  */
 use NFePHP\MDFe\Mail;
+use PHPUnit_Framework_TestCase;
 
 class MailTest extends PHPUnit_Framework_TestCase
 {
     public $mail;
-    
+
     public function testInstanciar()
     {
         $configJson = file_get_contents(dirname(__FILE__) . '/fixtures/config/fakeconfig.json');
         $json = json_decode($configJson);
-        $aMail = (array) $json->aMailConf;
+        $aMail = (array)$json->aMailConf;
         $this->mail = new Mail($aMail);
     }
 }
