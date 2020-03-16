7b49943282852d897bce0d1daee323b632f65e5d
Vande Cavassin <cavassin.vanderlei@gmail.com>
2020-03-16 10:22:44 -0300

    Nota técnica 2020.001

--- examples/testaEnvio.php
+++ examples/testaEnvio.php
@@ -11,45 +11,43 @@ use NFePHP\MDFe\Common\Standardize;
 use NFePHP\MDFe\Tools;
 
 $config = [
-    "atualizacao" => "2019-06-15 08:29:21",
+    "atualizacao" => date('Y-m-d H:i:s'),
     "tpAmb" => 2,
-    "razaosocial" => "TG RIO SUL TRANSPORTES LTDA",
-    "siglaUF" => "SC",
-    "cnpj" => "05388274000113",
-    "inscricaomunicipal" => "11223",
-    "codigomunicipio" => "4204301",
-    "schemes" => "PL_MDFe_300a",
-    "versao" => "3.00"
+    "razaosocial" => 'FÁBRICA DE SOFTWARE MATRIZ',
+    "cnpj" => '',
+    "ie" => '',
+    "siglaUF" => 'PR',
+    "versao" => '3.00'
 ];
-header("Content-type: text/xml");
-try {
-    $caminhoCrt = "certificado.pfx";
-    $content = file_get_contents($caminhoCrt);
-    $certificate = Certificate::readPfx($content, '1234');
 
-    $xml = '<?xml version="1.0" encoding="UTF-8"?>
-    <MDFe xmlns="http://www.portalfiscal.inf.br/mdfe"><infMDFe Id="MDFe42200305388274000113580040000000111000000022" versao="3.00"><ide><cUF>42</cUF><tpAmb>2</tpAmb><tpEmit>1</tpEmit><tpTransp>1</tpTransp><mod>58</mod><serie>4</serie><nMDF>11</nMDF><cMDF>00000002</cMDF><cDV>2</cDV><modal>1</modal><dhEmi>2020-03-16T09:26:00-03:00</dhEmi><tpEmis>1</tpEmis><procEmi>0</procEmi><verProc>SysconWeb_1.0</verProc><UFIni>SC</UFIni><UFFim>SC</UFFim><infMunCarrega><cMunCarrega>4201257</cMunCarrega><xMunCarrega>APIUNA</xMunCarrega></infMunCarrega><dhIniViagem>2020-03-16T11:30:00-03:00</dhIniViagem></ide><emit><CNPJ>05388274000113</CNPJ><IE>254504671</IE><xNome>TG RIO SUL TRANSPORTES LTDA</xNome><xFant>TG RIO SUL TRANSPORTES LTDA</xFant><enderEmit><xLgr>AREA RURAL</xLgr><nro>0001</nro><xBairro>AREA RURAL</xBairro><cMun>4204301</cMun><xMun>CONCORDIA</xMun><CEP>89709000</CEP><UF>SC</UF><fone>49991804520</fone><email>CAVASSIN.VANDERLEI@GMAIL.COM</email></enderEmit></emit><infModal versaoModal="3.00"><rodo><infANTT><RNTRC>01299770</RNTRC><infContratante><CNPJ>09230232000372</CNPJ></infContratante><infPag><xNome>VANDE TESTE</xNome><CNPJ>04363243000145</CNPJ><Comp><tpComp>03</tpComp><vComp>134.55</vComp><xComp>descricao teste</xComp></Comp><vContrato>135.00</vContrato><indPag>1</indPag><infPrazo><vParcela>134.55</vParcela></infPrazo><infBanc><codBanco>001</codBanco><codAgencia>1</codAgencia></infBanc></infPag></infANTT><veicTracao><cInt>01</cInt><placa>DSA4854</placa><tara>8350</tara><capKG>8350</capKG><prop><CNPJ>05388274000113</CNPJ><RNTRC>01299770</RNTRC><xNome>TG RIO SUL TRANSPORTES LTDA</xNome><IE>254504671</IE><UF>SC</UF><tpProp>1</tpProp></prop><condutor><xNome>VANDERLEI CAVASSIN</xNome><CPF>07040551985</CPF></condutor><tpRod>03</tpRod><tpCar>02</tpCar><UF>SC</UF></veicTracao></rodo></infModal><infDoc><infMunDescarga><cMunDescarga>4204301</cMunDescarga><xMunDescarga>CONCORDIA</xMunDescarga><infCTe><chCTe>42200305388274000113570040000000111000000029</chCTe><indReentrega>1</indReentrega></infCTe><infCTe><chCTe>42200305388274000113570040000000121000000050</chCTe><indReentrega>1</indReentrega></infCTe></infMunDescarga></infDoc><seg><infResp><respSeg>1</respSeg></infResp><infSeg><xSeg>BRADESCO SEGUROS</xSeg><CNPJ>60746948000112</CNPJ></infSeg><nApol>99999</nApol><nAver>99999</nAver></seg><prodPred><tpCarga>03</tpCarga><xProd>produto teste</xProd></prodPred><tot><qCTe>2</qCTe><vCarga>1200.00</vCarga><cUnid>01</cUnid><qCarga>1.0000</qCarga></tot><lacres><nLacre>0000001</nLacre></lacres><autXML><CNPJ>04898488000177</CNPJ></autXML><infAdic><infAdFisco>FISCO</infAdFisco><infCpl>COMPLEMENTAR</infCpl></infAdic></infMDFe></MDFe>';
+try {
+    $certificate = Certificate::readPfx(
+        '',
+        ''
+    );
+
+    $xml = '<?xml version="1.0" encoding="UTF-8"?><MDFe xmlns="http://www.portalfiscal.inf.br/mdfe"><infMDFe Id="MDFe41190822545265000108580260000000081326846774" versao="3.00"><ide><cUF>41</cUF><tpAmb>2</tpAmb><tpEmit>2</tpEmit><mod>58</mod><serie>26</serie><nMDF>8</nMDF><cMDF>32684677</cMDF><cDV>4</cDV><modal>1</modal><dhEmi>2019-08-14T11:35:01-03:00</dhEmi><tpEmis>2</tpEmis><procEmi>0</procEmi><verProc>3.9.8</verProc><UFIni>PR</UFIni><UFFim>RS</UFFim><infMunCarrega><cMunCarrega>4108403</cMunCarrega><xMunCarrega>Francisco Beltrao</xMunCarrega></infMunCarrega><infPercurso><UFPer>SC</UFPer></infPercurso></ide><emit><CNPJ>22545265000108</CNPJ><IE>9069531021</IE><xNome>EMPRESA DEMONSTRACAO LTDA</xNome><xFant>FABRICA DE SOFTWARE MATRIZ</xFant><enderEmit><xLgr>AVENIDA JULIO ASSIS CAVALHEIRO</xLgr><nro>1</nro><xBairro>CENTRO</xBairro><cMun>4108403</cMun><xMun>Francisco Beltrao</xMun><CEP>85601000</CEP><UF>PR</UF><fone>4635230686</fone></enderEmit></emit><infModal versaoModal="3.00"><rodo xmlns="http://www.portalfiscal.inf.br/mdfe"><infANTT><RNTRC>12345678</RNTRC><infContratante><CPF>01234567890</CPF></infContratante></infANTT><veicTracao><placa>ABC1011</placa><RENAVAM>32132132131</RENAVAM><tara>0</tara><prop><CPF>01234567890</CPF><RNTRC>88888888</RNTRC><xNome>ALISSON</xNome><IE/><UF>PR</UF><tpProp>0</tpProp></prop><condutor><xNome>CLEITON</xNome><CPF>06844990960</CPF></condutor><tpRod>01</tpRod><tpCar>01</tpCar><UF>PR</UF></veicTracao><veicReboque><placa>ABC1012</placa><RENAVAM>12313213213</RENAVAM><tara>0</tara><capKG>20000</capKG><capM3>180</capM3><prop><CPF>01234567890</CPF><RNTRC>88888888</RNTRC><xNome>ALISSON</xNome><IE/><UF>PR</UF><tpProp>0</tpProp></prop><tpCar>03</tpCar><UF>PR</UF></veicReboque></rodo></infModal><infDoc><infMunDescarga><cMunDescarga>4314902</cMunDescarga><xMunDescarga>Porto Alegre</xMunDescarga><infNFe><chNFe>41190122545265000108550270000004491369658540</chNFe></infNFe></infMunDescarga><infMunDescarga><cMunDescarga>4300208</cMunDescarga><xMunDescarga>Ajuricaba</xMunDescarga><infNFe><chNFe>41190522545265000108550270000005731334929373</chNFe></infNFe></infMunDescarga></infDoc><tot><qNFe>2</qNFe><vCarga>72.04</vCarga><cUnid>01</cUnid><qCarga>3.0000</qCarga></tot><lacres><nLacre>3113213213213213213213</nLacre></lacres></infMDFe></MDFe>';
 
     $tools = new Tools(json_encode($config), $certificate);
     
     $xmlAssinado = $tools->signMDFe($xml);
 
+    header('Content-type: text/plain; charset=UTF-8');
+    echo $xmlAssinado;
     
-   // echo $xmlAssinado;
-    
-    $resp = $tools->sefazEnviaLote([$xmlAssinado], rand(1, 10000));
+    //$resp = $tools->sefazEnviaLote([$xmlAssinado], rand(1, 10000));
 
     
-    $st = new Standardize();
-    $std = $st->toStd($resp);
+    //$st = new Standardize();
+    //$std = $st->toStd($resp);
 
-    sleep(3);
+    //sleep(3);
 
-    $resp = $tools->sefazConsultaRecibo($std->infRec->nRec);
-    $std = $st->toStd($resp);
+    //$resp = $tools->sefazConsultaRecibo($std->infRec->nRec);
+    //$std = $st->toStd($resp);
 
     //echo '<pre>';
-    echo var_dump($std);
+    //print_r($std);
     //echo "</pre>";
 } catch (Exception $e) {
     echo $e->getMessage();
--- examples/testaMake.php
+++ examples/testaMake.php
@@ -5,18 +5,15 @@ ini_set('display_errors', 'On');
 require_once '../bootstrap.php';
 
 use NFePHP\MDFe\Make;
-use NFePHP\Common\Certificate;
-use NFePHP\MDFe\Common\Standardize;
-use NFePHP\MDFe\Tools;
 
 $config = [
     "atualizacao" => "2019-06-15 08:29:21",
     "tpAmb" => 2,
-    "razaosocial" => "TG RIO SUL TRANSPORTES LTDA",
-    "siglaUF" => "SC",
-    "cnpj" => "05388274000113",
+    "razaosocial" => "SOFTWARE & HARDWARE INFORMATICA - ME",
+    "siglaUF" => "SP",
+    "cnpj" => "11222333444455",
     "inscricaomunicipal" => "11223",
-    "codigomunicipio" => "4204301",
+    "codigomunicipio" => "3518800",
     "schemes" => "PL_MDFe_300a",
     "versao" => "3.00"
 ];
@@ -31,39 +28,38 @@ $mdfe->setOnlyAscii(true);
  */
 
 $std = new \stdClass();
-$std->cUF = '42';
-$std->tpAmb = '2';
+$std->cUF = '35';
+$std->tpAmb = '1';
 $std->tpEmit = '1';
-$std->tpTransp = '1';
+$std->tpTransp = 'ETC';
 $std->mod = '58';
-$std->serie = '4';
-$std->nMDF = '11';
-$std->cMDF = '00000002';
-$std->cDV = '2';
+$std->serie = '0';
+$std->nMDF = '5766';
+$std->cMDF = '00025563';
+$std->cDV = '5';
 $std->modal = '1';
-$std->dhEmi = '2020-03-16T09:26:00-03:00';
-$std->tpEmis = '1';
+$std->dhEmi = '2019-04-23T06:00:48-03:00';
+$std->tpEmis = '2';
 $std->procEmi = '0';
-$std->verProc = 'SysconWeb_1.0';
-$std->UFIni = 'SC';
-$std->UFFim = 'SC';
-$std->dhIniViagem = '2020-03-16T11:30:00-03:00';
-//$std->indCanalVerde = '1';
-//$std->indCarregaPosterior = '1';
+$std->verProc = '1.6';
+$std->UFIni = 'SP';
+$std->UFFim = 'PA';
+$std->dhIniViagem = '2019-04-23T06:00:48-03:00';
+$std->indCanalVerde = '1';
+$std->indCarregaPosterior = '1';
 $mdfe->tagide($std);
 
 // for {
 $infMunCarrega = new stdClass();
-$infMunCarrega->cMunCarrega = '4201257';
-$infMunCarrega->xMunCarrega = 'APIUNA';
+$infMunCarrega->cMunCarrega = '3518800';
+$infMunCarrega->xMunCarrega = 'GUARULHOS';
 $mdfe->taginfMunCarrega($infMunCarrega);
 // }
 
 // for
-/*
 $infPercurso = new \stdClass();
 $infPercurso->UFPer = "PR";
-$mdfe->taginfPercurso($infPercurso);*/
+$mdfe->taginfPercurso($infPercurso);
 // }
 
 /*
@@ -74,22 +70,22 @@ $mdfe->taginfPercurso($infPercurso);*/
  * Grupo emit ( Emitente )
  */
 $std = new \stdClass();
-$std->CNPJ = '05388274000113';
-$std->IE = '254504671';
-$std->xNome = 'TG RIO SUL TRANSPORTES LTDA';
-$std->xFant = 'TG RIO SUL TRANSPORTES LTDA';
+$std->CNPJ = '00000000000000';
+$std->IE = '111111111111';
+$std->xNome = 'SIMULADA LOGISTICA LTDA- EPP';
+$std->xFant = 'SIMULADA LOGISTICA LTDA- EPP';
 $mdfe->tagemit($std);
 
 $std = new \stdClass();
-$std->xLgr = 'AREA RURAL';
+$std->xLgr = 'RUA CENTRAL';
 $std->nro = '0001';
-$std->xBairro = 'AREA RURAL';
-$std->cMun = '4204301';
-$std->xMun = 'CONCORDIA';
-$std->CEP = '89709000';
-$std->UF = 'SC';
-$std->fone = '49991804520';
-$std->email = 'CAVASSIN.VANDERLEI@GMAIL.COM';
+$std->xBairro = 'CENTRO';
+$std->cMun = '3518800';
+$std->xMun = 'GUARULHOS';
+$std->CEP = '07000000';
+$std->UF = 'SP';
+$std->fone = '1125252424';
+$std->email = 'simulada@simulada.com.br';
 $mdfe->tagenderEmit($std);
 /*
  * fim emit
@@ -101,23 +97,20 @@ $mdfe->tagenderEmit($std);
 
 /* Grupo infANTT */
 $infANTT = new \stdClass();
-$infANTT->RNTRC = '01299770';
+$infANTT->RNTRC = '12345678';
 $mdfe->taginfANTT($infANTT);
 
 /* informações do CIOT */
 // for {
-/*
 $infCIOT = new \stdClass();
 $infCIOT->CIOT = '123456789012';
 $infCIOT->CPF = '11122233344';
 $infCIOT->CNPJ = '11222333444455';
 $mdfe->taginfCIOT($infCIOT);
-*/
 // }
 
 /* informações do Vale Pedágio */
 // for {
-/*
 $valePed = new \stdClass();
 $valePed->CNPJForn = '11222333444455';
 $valePed->CNPJPg = '66777888999900';
@@ -125,44 +118,13 @@ $valePed->CPFPg = '11122233355';
 $valePed->nCompra = '777778888999999';
 $valePed->vValePed = '100.00';
 $mdfe->tagdisp($valePed);
-*/
 // }
 
 /* informações do contratante */
 // for {
 $infContratante = new \stdClass();
-
 $infContratante->CNPJ = '09230232000372';
 $mdfe->taginfContratante($infContratante);
-
-
-$pagto = new \stdClass();
-$pagto->xNome = "VANDE TESTE";
-$pagto->CPF = "00000000000";
-$pagto->vContrato = "135.00";
-$pagto->indPag = 1;
-//$pagto->idEstrangeiro = "VANDE TESTE";
-
-$infCom = new \stdClass();
-$infCom->tpComp = '03';
-$infCom->vComp = '134.55';
-$infCom->xComp = 'descricao teste';
-$pagto->Comp = [$infCom];
-
-$infPrazo = new \stdClass();
-$infPrazo->vParcela = 134.55;
-$pagto->infPrazo = [$infPrazo];
-
-$banco = new \stdClass();
-$banco->codBanco = "001";
-$banco->codAgencia = "1";
-//$banco->CNPJIPEF = "39207541000120";
-$pagto->infBanc = [$banco];
-
-
-$mdfe->taginfPag($pagto);
-
-
 // }
 
 /* fim infANTT */
@@ -170,25 +132,25 @@ $mdfe->taginfPag($pagto);
 /* Grupo veicTracao */
 $veicTracao = new \stdClass();
 $veicTracao->cInt = '01';
-$veicTracao->placa = 'DSA4854';
+$veicTracao->placa = 'DBL6040';
 $veicTracao->tara = '8350';
 $veicTracao->capKG = '8350';
 $veicTracao->tpRod = '03';
 $veicTracao->tpCar = '02';
-$veicTracao->UF = 'SC';
+$veicTracao->UF = 'PA';
 
 $condutor = new \stdClass();
-$condutor->xNome = 'VANDERLEI CAVASSIN';
-$condutor->CPF = '07040551985';
+$condutor->xNome = 'JOAO DA SILVA';
+$condutor->CPF = '11122233344';
 $veicTracao->condutor = [$condutor];
 
 $prop = new \stdClass();
-$prop->CPF = '';
-$prop->CNPJ = '05388274000113';
-$prop->RNTRC = '01299770';
-$prop->xNome = 'TG RIO SUL TRANSPORTES LTDA';
-$prop->IE = '254504671';
-$prop->UF = 'SC';
+$prop->CPF = '01234567890';
+$prop->CNPJ = '';
+$prop->RNTRC = '12345678';
+$prop->xNome = 'JOAO DA SILVA';
+$prop->IE = '03857164';
+$prop->UF = 'PR';
 $prop->tpProp = '1';
 $veicTracao->prop = $prop;
 
@@ -196,43 +158,201 @@ $mdfe->tagveicTracao($veicTracao);
 
 /* fim veicTracao */
 
+/* Grupo veicReboque */
+$veicReboque = new \stdClass();
+$veicReboque->cInt = '02';
+$veicReboque->placa = 'XXX1111';
+$veicReboque->tara = '8350';
+$veicReboque->capKG = '15000';
+$veicReboque->tpCar = '02';
+$veicReboque->UF = 'SP';
+
+$prop = new \stdClass();
+$prop->CPF = '01234567890';
+$prop->CNPJ = '';
+$prop->RNTRC = '12345678';
+$prop->xNome = 'JOAO DA SILVA';
+$prop->IE = '03857164';
+$prop->UF = 'PR';
+$prop->tpProp = '1';
+$veicReboque->prop = $prop;
+
+$mdfe->tagveicReboque($veicReboque);
+/* fim veicReboque */
+
+$lacRodo = new \stdClass();
+$lacRodo->nLacre = '1502400';
+$mdfe->taglacRodo($lacRodo);
 /* fim rodo */
 
 /*
  * Grupo infDoc ( Documentos fiscais )
  */
 $infMunDescarga = new \stdClass();
-$infMunDescarga->cMunDescarga = '4204301';
-$infMunDescarga->xMunDescarga = 'CONCORDIA';
+$infMunDescarga->cMunDescarga = '1502400';
+$infMunDescarga->xMunDescarga = 'CASTANHAL';
 $infMunDescarga->nItem = 0;
 $mdfe->taginfMunDescarga($infMunDescarga);
 
-
+/* infCTe */
 $std = new \stdClass();
-$std->chCTe = '42200305388274000113570040000000111000000029';
-$std->indReentrega = '0';
-$std->nItem = 0;
-/*
-$std->chCTe = '42200305388274000113570040000000121000000050';
+$std->chCTe = '35310800000000000372570010001999091000027765';
+$std->SegCodBarra = '012345678901234567890123456789012345';
 $std->indReentrega = '1';
-$std->nItem = 1;*/
+$std->nItem = 0;
+
+/* Informações das Unidades de Transporte (Carreta/Reboque/Vagão) */
+$stdinfUnidTransp = new \stdClass();
+$stdinfUnidTransp->tpUnidTransp = '1';
+$stdinfUnidTransp->idUnidTransp = 'AAA-1111';
+
+/* Lacres das Unidades de Transporte */
+$stdlacUnidTransp = new \stdClass();
+$stdlacUnidTransp->nLacre = ['00000001', '00000002'];
+
+$stdinfUnidTransp->lacUnidTransp = $stdlacUnidTransp;
+
+/* Informações das Unidades de Carga (Containeres/ULD/Outros) */
+$stdinfUnidCarga = new \stdClass();
+$stdinfUnidCarga->tpUnidCarga = '1';
+$stdinfUnidCarga->idUnidCarga = '01234567890123456789';
+
+/* Lacres das Unidades de Carga */
+$stdlacUnidCarga = new \stdClass();
+$stdlacUnidCarga->nLacre = ['00000001', '00000002'];
+
+$stdinfUnidCarga->lacUnidCarga = $stdlacUnidCarga;
+$stdinfUnidCarga->qtdRat = '3.50';
+
+$stdinfUnidTransp->infUnidCarga = [$stdinfUnidCarga];
+$stdinfUnidTransp->qtdRat = '3.50';
+
+$std->infUnidTransp = [$stdinfUnidTransp];
+
+/* transporte de produtos classificados pela ONU como perigosos */
+$stdperi = new \stdClass();
+$stdperi->nONU = '1234';
+$stdperi->xNomeAE = 'testeNome';
+$stdperi->xClaRisco = 'testeClaRisco';
+$stdperi->grEmb = 'testegrEmb';
+$stdperi->qTotProd = '1';
+$stdperi->qVolTipo = '1';
+$std->peri = [$stdperi];
+
+/* Grupo de informações da Entrega Parcial (Corte de Voo) */
+$stdinfEntregaParcial = new \stdClass();
+$stdinfEntregaParcial->qtdTotal = '1234.56';
+$stdinfEntregaParcial->qtdParcial = '1234.56';
+$std->infEntregaParcial = $stdinfEntregaParcial;
 
 $mdfe->taginfCTe($std);
 
-
 $infMunDescarga = new \stdClass();
-$infMunDescarga->cMunDescarga = '4204301';
-$infMunDescarga->xMunDescarga = 'CONCORDIA';
-$infMunDescarga->nItem = 0;
+$infMunDescarga->cMunDescarga = '1502400';
+$infMunDescarga->xMunDescarga = 'CASTANHAL';
+$infMunDescarga->nItem = 1;
 $mdfe->taginfMunDescarga($infMunDescarga);
 
-$stda = new \stdClass();
-$stda->chCTe = '42200305388274000113570040000000121000000050';
-$stda->indReentrega = '0';
-$stda->nItem = 1;
+/* infCTe */
+$std = new \stdClass();
+$std->chCTe = '35310800000000000372570010001998991000614492';
+$std->nItem = 1;
+$mdfe->taginfCTe($std);
 
+/* infNFe */
 
-$mdfe->taginfCTe($stda);
+$std = new \stdClass();
+$std->chNFe = '35310800000000000372570010001999091000099999';
+$std->SegCodBarra = '012345678901234567890123456789012345';
+$std->indReentrega = '1';
+$std->nItem = 0;
+
+// Informações das Unidades de Transporte (Carreta/Reboque/Vagão)
+$stdinfUnidTransp = new \stdClass();
+$stdinfUnidTransp->tpUnidTransp = '1';
+$stdinfUnidTransp->idUnidTransp = 'AAA-1111';
+
+// Lacres das Unidades de Transporte
+$stdlacUnidTransp = new \stdClass();
+$stdlacUnidTransp->nLacre = ['00000001', '00000002'];
+
+$stdinfUnidTransp->lacUnidTransp = $stdlacUnidTransp;
+
+// Informações das Unidades de Carga (Containeres/ULD/Outros)
+$stdinfUnidCarga = new \stdClass();
+$stdinfUnidCarga->tpUnidCarga = '1';
+$stdinfUnidCarga->idUnidCarga = '01234567890123456789';
+
+// lacres das Unidades de Carga
+$stdlacUnidCarga = new \stdClass();
+$stdlacUnidCarga->nLacre = ['00000001', '00000002'];
+
+$stdinfUnidCarga->lacUnidCarga = $stdlacUnidCarga;
+$stdinfUnidCarga->qtdRat = '3.50';
+
+$stdinfUnidTransp->infUnidCarga = [$stdinfUnidCarga];
+$stdinfUnidTransp->qtdRat = '3.50';
+
+$std->infUnidTransp = [$stdinfUnidTransp];
+
+// transporte de produtos classificados pela ONU como perigosos
+$stdperi = new \stdClass();
+$stdperi->nONU = '1234';
+$stdperi->xNomeAE = 'testeNome';
+$stdperi->xClaRisco = 'testeClaRisco';
+$stdperi->grEmb = 'testegrEmb';
+$stdperi->qTotProd = '1';
+$stdperi->qVolTipo = '1';
+$std->peri = [$stdperi];
+
+$mdfe->taginfNFe($std);
+
+/* infMDFeTransp */
+
+$std = new \stdClass();
+$std->chMDFe = '35310800000000000372570010001999091000088888';
+$std->indReentrega = '1';
+$std->nItem = 0;
+
+// Informações das Unidades de Transporte (Carreta/Reboque/Vagão)
+$stdinfUnidTransp = new \stdClass();
+$stdinfUnidTransp->tpUnidTransp = '1';
+$stdinfUnidTransp->idUnidTransp = 'AAA-1111';
+
+// Lacres das Unidades de Transporte
+$stdlacUnidTransp = new \stdClass();
+$stdlacUnidTransp->nLacre = ['00000001', '00000002'];
+
+$stdinfUnidTransp->lacUnidTransp = $stdlacUnidTransp;
+
+// Informações das Unidades de Carga (Containeres/ULD/Outros)
+$stdinfUnidCarga = new \stdClass();
+$stdinfUnidCarga->tpUnidCarga = '1';
+$stdinfUnidCarga->idUnidCarga = '01234567890123456789';
+
+// lacres das Unidades de Carga
+$stdlacUnidCarga = new \stdClass();
+$stdlacUnidCarga->nLacre = ['00000001', '00000002'];
+
+$stdinfUnidCarga->lacUnidCarga = $stdlacUnidCarga;
+$stdinfUnidCarga->qtdRat = '3.50';
+
+$stdinfUnidTransp->infUnidCarga = [$stdinfUnidCarga];
+$stdinfUnidTransp->qtdRat = '3.50';
+
+$std->infUnidTransp = [$stdinfUnidTransp];
+
+// transporte de produtos classificados pela ONU como perigosos
+$stdperi = new \stdClass();
+$stdperi->nONU = '1234';
+$stdperi->xNomeAE = 'testeNome';
+$stdperi->xClaRisco = 'testeClaRisco';
+$stdperi->grEmb = 'testegrEmb';
+$stdperi->qTotProd = '1';
+$stdperi->qVolTipo = '1';
+$std->peri = [$stdperi];
+
+$mdfe->taginfMDFeTransp($std);
 
 /* fim grupo infDoc */
 
@@ -242,29 +362,20 @@ $std->respSeg = '1';
 
 /* Informações da seguradora */
 $stdinfSeg = new \stdClass();
-$stdinfSeg->xSeg = 'BRADESCO SEGUROS';
-$stdinfSeg->CNPJ = '60746948000112';
+$stdinfSeg->xSeg = 'SOMPO SEGUROS';
+$stdinfSeg->CNPJ = '11222333444455';
 
 $std->infSeg = $stdinfSeg;
-$std->nApol = '99999';
-$std->nAver = ['99999'];
+$std->nApol = '11223344555';
+$std->nAver = ['0572012190000000000007257001000199899140', '0572012190000000000007257001000199708140'];
 $mdfe->tagseg($std);
 /* fim grupo Seguro */
 
-$prodpre = new \stdClass();
-$prodpre->tpCarga = "03";
-$prodpre->xProd = "produto teste";
-//$prodpre->cEAN = "";
-//$prodpre->NCM = "0123456";
-$mdfe->tagprodPred($prodpre);
-
-
-
 /* grupo de totais */
 $std = new \stdClass();
-$std->vCarga = '1200.00';
+$std->vCarga = '580042.92';
 $std->cUnid = '01';
-$std->qCarga = '1.0000';
+$std->qCarga = '35454.9400';
 $mdfe->tagtot($std);
 /* fim grupo de totais */
 
@@ -279,46 +390,17 @@ $mdfe->taglacres($std);
 /* grupo Autorizados para download do XML do DF-e */
 // for {
 $std = new \stdClass();
-$std->CNPJ = '04898488000177';
+$std->CNPJ = '11122233344455';
 $mdfe->tagautXML($std);
 // }
 
 /* grupo Informações Adicionais */
 $std = new \stdClass();
-$std->infCpl = 'COMPLEMENTAR';
-$std->infAdFisco = 'FISCO';
+$std->infCpl = 'Contrato No 007018 2 CARR BBB1111';
+$std->infAdFisco = 'Contrato No 007018 2 CARR BBB1111';
 $mdfe->taginfAdic($std);
 /* fim grupo Informações Adicionais */
 
-try {
-    //$xml = $mdfe->getXML(); // O conteúdo do XML fica armazenado na variável $xml
-    header("Content-type: text/xml");
-    $xml = $mdfe->getXML(); 
-
-
-    echo $xml;exit;
-    $caminhoCrt = "certificado.pfx";
-    $content = file_get_contents($caminhoCrt);
-    $certificate = Certificate::readPfx($content, '1234');
-    $tools = new Tools(json_encode($config), $certificate);
-    $xmlAssinado = $tools->signMDFe($xml);
-
-    //echo $xmlAssinado;
-    $resp = $tools->sefazEnviaLote([$xmlAssinado], rand(1, 10000));
-
-    
-    $st = new Standardize();
-    $std = $st->toStd($resp);
-
-    sleep(3);
-
-    $resp = $tools->sefazConsultaRecibo($std->infRec->nRec);
-    $std = $st->toStd($resp);
-
-    echo '<pre>';
-    print_r($std);
-    echo "</pre>";
-} catch (Exception $e) {
-    echo $e->getMessage();
-    var_dump($mdfe->getErrors());
-}
+$xml = $mdfe->getXML(); // O conteúdo do XML fica armazenado na variável $xml
+header("Content-type: text/xml");
+echo $mdfe->getXML();
